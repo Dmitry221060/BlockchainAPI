77f34917ef1d429562294df10acfb139d9123136
Dmitry221060 <ya.dima42103ya@yandex.ru>
2025-09-19 03:35:46 +0500

    Update env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
 import { CosmosModule } from "./cosmos/cosmos.module";
 import { EVMModule } from "./evm/evm.module";
 import { HealthModule } from "./health/health.module";
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     CosmosModule,
     EVMModule,
     HealthModule,
--- test/utils/create-app.ts
+++ test/utils/create-app.ts
@@ -1,20 +1,26 @@
 import { ValidationPipe } from "@nestjs/common";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 import { Test } from "@nestjs/testing";
 import { NestExpressApplication } from "@nestjs/platform-express";
 import "tsconfig-paths/register";
-import config from "src/config";
 import { AppModule } from "src/app.module";
 
 export async function createApp() {
   const moduleRef = await Test.createTestingModule({
-    imports: [AppModule],
+    imports: [
+      ConfigModule.forFeature(() => ({
+        COSMOS_RPC_NODE: "https://cosmos-rpc.publicnode.com:443",
+      })),
+      AppModule,
+    ],
   }).compile();
 
   const app = moduleRef.createNestApplication<NestExpressApplication>();
+  const config = app.get(ConfigService);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   await app.init();
 
-  await app.listen(config.server.port);
+  await app.listen(config.getOrThrow<number>("APP_PORT"));
 
   return app;
 }
