5c44e93c048a69fc845c67e7d4d65cfe9a7abb3c
Dmitry221060 <ya.dima42103ya@yandex.ru>
2025-09-19 06:36:03 +0500

    Allow hooking test module creation for mocking

--- test/utils/create-app.ts
+++ test/utils/create-app.ts
@@ -1,21 +1,34 @@
 import { ValidationPipe } from "@nestjs/common";
 import { ConfigModule, ConfigService } from "@nestjs/config";
-import { Test } from "@nestjs/testing";
+import { Test, TestingModuleBuilder } from "@nestjs/testing";
 import { NestExpressApplication } from "@nestjs/platform-express";
 import "tsconfig-paths/register";
 import { AppModule } from "src/app.module";
 
-export async function createApp() {
-  const moduleRef = await Test.createTestingModule({
+export interface CreateAppOptions {
+  moduleBuilderHook?: (
+    moduleBuilder: TestingModuleBuilder,
+  ) => TestingModuleBuilder;
+}
+
+export async function createApp(options: CreateAppOptions = {}) {
+  let moduleRef = Test.createTestingModule({
     imports: [
       ConfigModule.forFeature(() => ({
+        EVM_RPC_NODE: "https://sei-evm-rpc.publicnode.com",
         COSMOS_RPC_NODE: "https://cosmos-rpc.publicnode.com:443",
       })),
       AppModule,
     ],
-  }).compile();
+  });
 
-  const app = moduleRef.createNestApplication<NestExpressApplication>();
+  if (options.moduleBuilderHook) {
+    moduleRef = options.moduleBuilderHook(moduleRef);
+  }
+
+  const moduleFixture = await moduleRef.compile();
+
+  const app = moduleFixture.createNestApplication<NestExpressApplication>();
   const config = app.get(ConfigService);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   await app.init();
